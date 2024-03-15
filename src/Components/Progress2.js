210ee21634fc06fcc2ed51c3d031cf0a8cead4b9
NithinGanna <116946895+NithinGanna@users.noreply.github.com>
2024-03-16 00:13:26 +0530

    Update Progress2.js

--- src/Components/Progress2.js
+++ src/Components/Progress2.js
@@ -88,7 +88,7 @@ const OverallActivitiesPieChart = () => {
                 {isLoading ? (
                     <p>Loading...</p>
                 ) : (
-                    <canvas id="activitiesPieChart" style={{ width: '100%', height: '80%' }}></canvas>
+                    <canvas id="activitiesPieChart" style={{ width: '100%', height: '400px' }}></canvas>
                 )}
             </div>
         </div>
