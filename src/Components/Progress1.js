2730cabdc07dd3d53d8a97b53177a08d8039ad29
NithinGanna <116946895+NithinGanna@users.noreply.github.com>
2024-03-15 23:27:55 +0530

    Update Progress1.js

--- src/Components/Progress1.js
+++ src/Components/Progress1.js
@@ -107,7 +107,7 @@ const DateWiseActivitiesBarGraph = () => {
                 </div>
             ) : (
                 <div style={{ width: '100%', maxWidth: '40%', margin: 'auto' }}>
-                    <canvas id="activitiesBarGraph" style={{ width: '100%', height: '100%' }}></canvas>
+                    <canvas id="activitiesBarGraph" style={{ width: 'auto', height: 'auto' }}></canvas>
                 </div>
             )}
         </div>
