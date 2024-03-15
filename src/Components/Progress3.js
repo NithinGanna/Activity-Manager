576cb100f64aa6582be46403dbe5c6cf15f63900
NithinGanna <116946895+NithinGanna@users.noreply.github.com>
2024-03-16 00:18:59 +0530

    Update Progress3.js

--- src/Components/Progress3.js
+++ src/Components/Progress3.js
@@ -87,7 +87,7 @@ const OverallActivitiesBarGraph = () => {
                 </div>
             ) : (
                 <div style={{ width: '100%', maxWidth: '600px', margin: 'auto' }}>
-                    <canvas id="activitiesBarGraph" style={{ width: '100%', height: '500px' }}></canvas>
+                    <canvas id="activitiesBarGraph" style={{ width: '100%', height: '600px' }}></canvas>
                 </div>
             )}
         </div>
